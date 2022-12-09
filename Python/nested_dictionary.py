6a28ed1a81b59c313c08f87286c755f6f54f0656
Yuri Kim <42161179+yrk3434@users.noreply.github.com>
2022-12-09 13:14:31 +0900

    Create nested_dictionary.py

---
+++ Python/nested_dictionary.py
@@ -0,0 +1,12 @@
+'''
+여러 계층의 딕셔너리 element 추가
+'''
+import collections.abc
+
+def update(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
