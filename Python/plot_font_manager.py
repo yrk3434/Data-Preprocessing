3aff645cb39f9c7403508c78712bf4dfaa47384f
Yuri Kim <42161179+yrk3434@users.noreply.github.com>
2023-01-18 15:25:15 +0900

    Update plot_font_manager.py

--- Python/plot_font_manager.py
+++ Python/plot_font_manager.py
@@ -2,7 +2,9 @@
 한글폰트가 없는 서비스에서 그래프를 그릴 때
 한글 폰트를 저장 후 적용
 '''
-
+import matplotlib
 from matplotlib import font_manager
 font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+matplotlib.rc('font',family=font_name)
 font_manager.fontManager.addfont(font_path)
